dd47d08e2b1f2963edd985fc2199539e3535498e
James <jaimersonaraujo@gmail.com>
2017-12-27 10:19:40 -0300

    Use sane measurement units instead of magic ones

--- lib/hashtag_print/renderer.rb
+++ lib/hashtag_print/renderer.rb
@@ -1,4 +1,5 @@
 require 'prawn'
+require 'prawn/measurement_extensions'
 
 # Renders a HashtagPrint::Document to a PDF with the given file name.
 module HashtagPrint
@@ -6,7 +7,7 @@ module HashtagPrint
     def self.render(document)
       path = output_path(document.image_digest)
 
-      Prawn::Document.generate(path) do
+      Prawn::Document.generate(path, margin: [2.5.mm,2.5.mm,2.5.mm,2.5.mm], page_size: [10.cm, 15.cm]) do
         font_families.update({
           'QuattrocentoSans' => {
             normal: 'fonts/QuattrocentoSans-Regular.ttf',
@@ -14,23 +15,23 @@ module HashtagPrint
           }
         })
 
-        bounding_box([0, cursor], width: 550, height: 730) do
+        bounding_box([0, cursor], width: 95.mm, height: 145.mm) do
           transparent(1, 0.3) do
             stroke_bounds
           end
 
           font_size 16
 
-          image document.image, position: :center, fit: [550, 475]
+          image document.image, position: :center, fit: [95.mm, 95.mm]
 
-          indent(20) do
-            move_down 20
+          indent(2.mm) do
+            move_down 5.mm
 
             font('QuattrocentoSans', style: :bold) do
               text document.user_name, color: 'F03030'
             end
 
-            move_down 20
+            move_down 5.mm
 
             font 'QuattrocentoSans'
             text document.formatted_caption, inline_format: true
